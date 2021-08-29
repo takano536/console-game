3c666d99116f726797d6622151d8a8e356109735
takano536 <ohnishi.takanori@gmail.com>
2021-08-29 18:15:46 +0900

    change color

--- console_game/Goal.cpp
+++ console_game/Goal.cpp
@@ -7,7 +7,7 @@ Goal::Goal(short x, short y)
 	curr_pos = COORD{x, y};
 	prev_pos = COORD{x, y};
 	graphic = 'G';
-	foreground_color = Color::BLACK;
-	background_color = Color::LIGHT_SKY_BLUE;
+	foreground_color = Color::WHITE;
+	background_color = Color::BLUE;
 	hp = -1;
 }
